a2eca932c29ed7670b4fd9f9d56658e8a55f16cd
Gauravshrama <GS173909@GMAIL.COM>
2025-08-23 21:12:09 +0530

    Initial commit : java project setup

---
+++ webdaynamic/src/main/java/com/example/webdaynamic/WebdaynamicApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdaynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdaynamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdaynamicApplication.class, args);
+	}
+
+}
---
+++ webdaynamic/src/test/java/com/example/webdaynamic/WebdaynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdaynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdaynamicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
